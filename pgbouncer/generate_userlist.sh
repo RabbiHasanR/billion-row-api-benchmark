89fdc3d733bb236c4060b8e7cdbe2aa19b506f54
rabbi <jasrabbi50@gmail.com>
2025-06-15 22:22:06 +0600

    api -> pgbouncer -> pgpool -> postgresql master and slave   done

--- pgbouncer/generate_userlist.sh
+++ pgbouncer/generate_userlist.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-
-# Fail if any variable is unset
 set -eu
 
-# Generate MD5 hash: md5(password + username)
-HASH=$(printf "%s%s" "$POSTGRES_PASSWORD" "$POSTGRES_USER" | md5sum | awk '{print $1}')
+# Generate MD5 hash in PostgreSQL format: md5(md5(password + username) + username)
+HASH=$(printf "md5%s" $(printf "%s%s" "$POSTGRES_PASSWORD" "$POSTGRES_USER" | md5sum | awk '{print $1}'))
 
 # Output to userlist.txt
-echo "\"$POSTGRES_USER\" \"md5$HASH\"" > /etc/pgbouncer/userlist.txt
+echo "\"$POSTGRES_USER\" \"$HASH\"" > /etc/pgbouncer/userlist.txt
 echo "✅ Generated userlist.txt for PgBouncer"
